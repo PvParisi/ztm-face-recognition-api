4b9dadc472e776d8d4a70a4db0e2b68ebf08d904
Piervincenzo Parisi <piervi_parisi@yahoo.it>
2018-03-18 12:13:23 +0100

    added api endpoints

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// it's just an example for developing purpose,
+// we are gonna use an actual db eventually!
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@email.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@email.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    if(req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('success');
+        } else {
+            res.status(400).json('error logging in');
+        }
+    res.json('signin');
+})
+
+app.post('/register', (req, res) => {
+    const {email, name, password} = req.body;
+    database.users.push(
+        {
+            id: '125',
+            name: name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()
+        }
+    );
+    res.json(database.users[database.users.length - 1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    database.users.forEach(user => {
+        if(user.id === id) {
+            return res.json(user);
+        }
+    });
+
+    res.status(400).json('no such user');
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            user.entries++;
+            return res.json(user.entries);
+        }
+    });
+
+    res.status(400).json('no such user');
+})
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000')
+})
