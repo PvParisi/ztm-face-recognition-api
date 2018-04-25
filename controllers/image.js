77ca3c05bc680d53b0635e1aceecebd55b0a2a12
Piervincenzo Parisi <piervi_parisi@yahoo.it>
2018-04-25 12:16:10 +0200

    moved clarifai to back end

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const API_KEY = 'c8fb5f6b2d30465badfe7aa7ee131bc6';
+
+const app = new Clarifai.App({
+    apiKey: API_KEY
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -9,4 +25,7 @@ const handleImage = (req, res, db) => {
         .catch(err => res.status(400).json('unable to get entries'))
 }
 
-module.exports = { handleImage }
+module.exports = { 
+    handleImage,
+    handleApiCall
+}
